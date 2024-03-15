5088e1fb0ff4d3dcb71d2d5fb274532cdb6e1982
julianchavez14 <161876900+julianchavez14@users.noreply.github.com>
2024-03-15 10:57:22 -0700

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -1,13 +1,20 @@
 # this is the customer class 
+'''
+this Customer class gets the customer login inforamtion and stores their username and password. Customer_info stores things
+like the customers Name,Phone number, Email, Payment information. Set_user_info and set_user_login sets their information and
+login credentials to allow access to the system.
 
-class Customer(self, login):
+'''
+
+class Customer:
+  
   def __init__(self):
         self.login = ""
         self.customer_info = ""
 
-    def set_user_info(self, info):
+  def set_user_info(self, info):
         self.customer_info = info
 
-    def set_user_login(self, login):
+  def set_user_login(self, login):
         self.login = login
-  
+
