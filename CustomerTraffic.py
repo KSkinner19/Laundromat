289c3f2f766cb3f9f196f34896443bec9faf39d8
wakandafrash <163576296+wakandafrash@users.noreply.github.com>
2024-03-15 11:14:53 -0700

    Update CustomerTraffic.py

--- CustomerTraffic.py
+++ CustomerTraffic.py
@@ -1,1 +1,17 @@
 
+class CustomerTraffic:
+  '''
+this Customer class gets the customer login inforamtion and stores their username and password. Customer_info stores things
+like the customers Name,Phone number, Email, Payment information. Set_user_info and set_user_login sets their information and
+login credentials to allow access to the system.
+'''
+  def __init__(self, users=0, machines_in_use=0):
+    self.users = users
+    self.machines_in_use = machines_in_use
+
+  def current_traffic(self,):
+    
+
+  def traffic_sumamry(self, users):
+    
+        
