70b97685db44f7dd6763293f9ac103282a3fd762
ojelabi09 <163576051+ojelabi09@users.noreply.github.com>
2024-03-15 11:27:36 -0700

    MachineAvailability.py

--- MachineAvailability.py
+++ MachineAvailability.py
@@ -2,21 +2,35 @@ class MachineAvailability :
    '''
 This MachineAvailability class represents which machine is in use and which isnt. It displays a status to the user for which machines 
 they can and can't use, including cost, time remaining and load size of each machine.
-'''
+''' class MachineAvailability :
+  
    def __init__ (self, number, machine_type, load_size): #initialize machineavailibility with attributes
-     #arguments: number represents # representing certain machine, machine_type represents washer or dryer load_size represents load size of machine
-     self.number = number
-     self.machine_type = machine_type
-     self.load_size = load_size
-     self.cost = 0.0
-     self.in_use = False
-     self.time_remaining = 0
-   def set_cost(self, base_amount, addiitonal_amount):
+     #arguments:   
+     self.number = number #number represents  certain machine
+     self.machine_type = machine_type #machine_type represents washer or dryer
+     self.load_size = load_size #load_size represents load size of machine
+     self.cost = 0.0 #cost of service
+     self.in_use = False #machine in use or not
+     self.time_remaining = 0 #time remaining to finish machine task
+  
+  def set_cost(self, base_amount, addiitonal_amount):
+   #calculate cost to use machine 
+   #base amount cost of using machine additional amount = amount contingent on load size. represented in float
      self.cost = base_amount + additional_amount * self.load_size
   
    def set_availability(self, availibility):
+    #availiibility status of machine
      self.in_use = availibility
 
    def machine_status(self): 
+    #return respnse saying whther available for use or not
      if self.in_use:
-       return
+      return f" Machine(self.number) is currently in use")
+     else:
+      return f" Machine(self.number) is currently available for use")
+   def set_time_remaining(self,time):
+    #set time remaining to complete task
+    self.time_remaining = time
+    #return message indicating time remaining for machine
+    return f"Machine {self.number} has {self_time_remaining} minutes remaining until complete
+  
