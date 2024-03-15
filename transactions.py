0cadc9dc06a4dc3f57fe55084cdba4e2c0313e57
Kaydon <116392992+KSkinner19@users.noreply.github.com>
2024-03-15 11:03:00 -0700

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -1,1 +1,14 @@
+import time
 
+class Transactions:
+  def __init__(self, cost, time, pay):
+    self.cost = cost
+    self.time = time
+    self.pay = pay
+
+def get_cost(washer, dryer):
+  total = float(washer + dryer)
+  return f"Total cost: ${total:.2f}"
+
+def time_track():
+  return time.strftime("%H:%M")
