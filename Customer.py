a4d728b3894db072ad3ddeed9eb6767b1674fac6
julianchavez14 <161876900+julianchavez14@users.noreply.github.com>
2024-03-15 10:45:07 -0700

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -1,1 +1,13 @@
 # this is the customer class 
+
+class Customer(self, login):
+  def __init__(self):
+        self.login = ""
+        self.customer_info = ""
+
+    def set_user_info(self, info):
+        self.customer_info = info
+
+    def set_user_login(self, login):
+        self.login = login
+  
