120c10688984310ad3a9cb046fa7bb9ea3f3f56c
julianchavez14 <161876900+julianchavez14@users.noreply.github.com>
2024-04-19 10:38:36 -0700

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -4,13 +4,27 @@ this Customer class gets the customer login inforamtion and stores their usernam
 like the customers Name,Phone number, Email, Payment information. Set_user_info and set_user_login sets their information and
 login credentials to allow access to the system.
 '''
-  def __init__(self):
+  def __init__(self, name, laundry_weight):
         self.login = ""
         self.customer_info = ""
+        self.name = name
+        self.laundry_weight = laundry_weight
+        self.notifications = []
 
   def set_user_info(self, info):
         self.customer_info = info
 
   def set_user_login(self, login):
         self.login = login
+    
+  def add_notification(self, notification):
+        self.notifications.append(notification)
+
+    def view_notifications(self):
+        return self.notifications
+
+class Notification:
+    def __init__(self, message, timestamp):
+        self.message = message
+        self.timestamp = timestamp
 
