2e0e3109c98f15c20876a448c6414db8d14dc1fc
julianchavez14 <161876900+julianchavez14@users.noreply.github.com>
2024-05-09 22:22:29 -0700

    Update MachineAvailability.py
    worked together on this py in replit

--- MachineAvailability.py
+++ MachineAvailability.py
@@ -1,36 +1,47 @@
-class MachineAvailability :
+
    '''
 This MachineAvailability class represents which machine is in use and which isnt. It displays a status to the user for which machines 
 they can and can't use, including cost, time remaining and load size of each machine.
-''' class MachineAvailability :
-  
-   def __init__ (self, number, machine_type, load_size): #initialize machineavailibility with attributes
-     #arguments:   
-     self.number = number #number represents  certain machine
-     self.machine_type = machine_type #machine_type represents washer or dryer
-     self.load_size = load_size #load_size represents load size of machine
-     self.cost = 0.0 #cost of service
-     self.in_use = False #machine in use or not
-     self.time_remaining = 0 #time remaining to finish machine task
-  
-  def set_cost(self, base_amount, addiitonal_amount):
-   #calculate cost to use machine 
-   #base amount cost of using machine additional amount = amount contingent on load size. represented in float
-     self.cost = base_amount + additional_amount * self.load_size
-  
-   def set_availability(self, availibility):
-    #availiibility status of machine
-     self.in_use = availibility
+''' 
+   class MachineAvailability:
+    def __init__(self, number, machine_type, load_size):
+        self.number = number
+        self.machine_type = machine_type
+        self.load_size = load_size
+        self.cost = 0.0 
+        self.additional_cost = 0.0
+        self.in_use = False
+        self.time_remaining = 0
 
-   def machine_status(self): 
-    #return respnse saying whther available for use or not
-     if self.in_use:
-      return f"(Machine(self.number) is currently in use"))
-     else:
-      return f"(Machine(self.number) is currently available for use"))
-   def set_time_remaining(self,time):
-    #set time remaining to complete task
-    self.time_remaining = time
-    #return message indicating time remaining for machine
-    return f"(Machine {self.number} has {self_time_remaining} minutes remaining until complete")
-  
+    def set_cost(self, base_amount, additional_amount=0.0):
+        self.cost = base_amount  # Store the updated base cost
+        self.additional_cost = additional_amount
+
+    def set_availability(self, availability):
+        self.in_use = availability
+        if not availability:
+            self.time_remaining = 0  
+            # Reset time remaining when 
+            #the machine becomes available
+
+
+    def machine_status(self):
+            def machine_status(self):
+                if self.in_use:
+                    status_message = f"Machine {self.number} is currently in use. "
+                    time_message = f"Time remaining: {self.time_remaining} minutes."
+                    return status_message + time_message
+                else:
+                    return f"Machine {self.number} is currently available for use."
+
+    def set_time_remaining(self, time):
+        self.time_remaining = time
+        return f"Machine {self.number} has {self.time_remaining} " \
+           "minutes remaining until done."
+
+    def decrease_time_remaining(self):
+        if self.in_use and self.time_remaining > 0:
+            self.time_remaining -= 1
+
+    def get_cost(self):
+        return self.cost + self.additional_cost
